c9a00aa20b55dbd17eb12529eb86cb132e7215fa
mateuszmagier <mateuszmagier@bitbucket.org>
2018-09-17 23:51:40 +0200

    Add very simple menu.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let options = {
+const MainMenu = function () {
+    var mainMenuContainer = null,
+        classicModeButton = null,
+        endlessModeButton = null;
 
-    };
-    const game = new Rullo(
-        document.querySelector(".game-container"),
-        options
-    );
+    var initClassicGame = function () {
+        const gameContainer = document.createElement("div");
+        gameContainer.classList.add("game-container");
+        document.querySelector(".container").appendChild(gameContainer);
+        document.querySelector(".container").classList.add("container--visible");
+
+        let options = {
+
+        };
+        const game = new Rullo(
+            document.querySelector(".game-container"),
+            options
+        );
+    }
+
+    mainMenuContainer = document.querySelector(".main-menu-container");
+    classicModeButton = document.querySelector(".classic-mode");
+    classicModeButton.addEventListener("click", function () {
+        mainMenuContainer.classList.add("main-menu-container--invisible");
+        initClassicGame();
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const mainMenu = MainMenu();
 });
