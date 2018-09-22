c4227cb29b098da46d44e91f0fb0820fe2a11d25
mateuszmagier <mateuszmagier@bitbucket.org>
2018-09-22 19:48:17 +0200

    Add animations to main menu.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const MainMenu = function () {
-    var mainMenuContainer = null,
+    var menuContainer = null,
+        mainMenu = null,
+        rulloLogo = null,
         classicModeButton = null,
+        classicMenu = null,
         endlessModeButton = null;
 
     var initClassicGame = function () {
@@ -18,11 +21,38 @@ const MainMenu = function () {
         );
     }
 
-    mainMenuContainer = document.querySelector(".main-menu-container");
+    menuContainer = document.querySelector(".menu-container");
+    mainMenu = document.querySelector(".main-menu");
+    rulloLogo = document.querySelector(".rullo-logo");
     classicModeButton = document.querySelector(".classic-mode");
-    classicModeButton.addEventListener("click", function () {
-        mainMenuContainer.classList.add("main-menu-container--invisible");
+    classicMenu = document.querySelector(".classic-menu");
+    endlessModeButton = document.querySelector(".endless-mode");
+    
+    function hideMainMenu() {
+        mainMenu.classList.add("main-menu--invisible");
+        classicMenu.classList.remove("classic-menu--invisible");
+    }
+    
+    function hideMenuContainer() {
+        menuContainer.classList.add("menu-container--invisible");
         initClassicGame();
+    }
+    
+    classicModeButton.addEventListener("click", function () {
+        this.classList.add("classic-mode--invisible");
+        endlessModeButton.classList.add("endless-mode--invisible");
+        
+        this.addEventListener("animationend", hideMainMenu); // hide main menu when animation is done
+//        initClassicGame();
+    });
+    
+    endlessModeButton.addEventListener("click", function () {
+        this.classList.add("endless-mode--invisible");
+        classicModeButton.classList.add("classic-mode--invisible");
+        rulloLogo.classList.add("rullo-logo--invisible");
+        
+        this.addEventListener("animationend", hideMenuContainer); // hide main menu container when animation is done
+//        initClassicGame();
     });
 }
 
