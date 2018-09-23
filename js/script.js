67070453763b617a4e639e6facec30979c4674c3
mateuszmagier <mateuszmagier@bitbucket.org>
2018-09-23 10:48:24 +0200

    Add classic mode menu.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,54 @@
+const ClassicMenu = function () {
+    var classicMenu = null,
+        ranges = null,
+        chosenRange = null,
+        chosenRangeIndex = 1,
+        chosenRangeText = "1-9",
+        choseRangeLine = null,
+        sizes = null,
+        chosenSize = null,
+        chosenSizeText = "";
+
+    classicMenu = document.querySelector(".classic-menu");
+    ranges = classicMenu.querySelectorAll(".range");
+    chosenRange = classicMenu.querySelector(".range--chosen");
+    choseRangeLine = classicMenu.querySelector(".chosen-range-line");
+    sizes = classicMenu.querySelectorAll(".size");
+
+    function registerRangeClickListeners() {
+        [].forEach.call(ranges, function (range) {
+            range.addEventListener("click", function () {
+                //                let newChosenRange = this;
+                if (chosenRange !== this) { // range change
+                    let newChosenRangeIndex = this.dataset.rangeIndex;
+                    choseRangeLine.classList.remove("chosen-range-line-" + chosenRangeIndex);
+                    chosenRangeIndex = newChosenRangeIndex;
+                    choseRangeLine.classList.add("chosen-range-line-" + chosenRangeIndex);
+
+                    chosenRange.classList.remove("range--chosen");
+                    chosenRange = this;
+                    chosenRangeText = chosenRange.innerText;
+                    chosenRange.classList.add("range--chosen");
+                }
+            });
+        });
+    }
+
+    function registerSizeClickListeners() {
+        [].forEach.call(sizes, function (size) {
+            size.addEventListener("click", function () {
+                chosenSize = this;
+                chosenSizeText = chosenSize.innerText;
+                console.log("Wybrane ustawienia: RANGE: " + chosenRangeText + ", SIZE: " + chosenSizeText);
+            });
+        });
+    }
+
+    registerRangeClickListeners();
+    registerSizeClickListeners();
+}
+
+
 const MainMenu = function () {
     var menuContainer = null,
         mainMenu = null,
@@ -27,36 +78,37 @@ const MainMenu = function () {
     classicModeButton = document.querySelector(".classic-mode");
     classicMenu = document.querySelector(".classic-menu");
     endlessModeButton = document.querySelector(".endless-mode");
-    
+
     function hideMainMenu() {
         mainMenu.classList.add("main-menu--invisible");
         classicMenu.classList.remove("classic-menu--invisible");
     }
-    
+
     function hideMenuContainer() {
         menuContainer.classList.add("menu-container--invisible");
         initClassicGame();
     }
-    
+
     classicModeButton.addEventListener("click", function () {
         this.classList.add("classic-mode--invisible");
         endlessModeButton.classList.add("endless-mode--invisible");
-        
+
         this.addEventListener("animationend", hideMainMenu); // hide main menu when animation is done
-//        initClassicGame();
+        //        initClassicGame();
     });
-    
+
     endlessModeButton.addEventListener("click", function () {
         this.classList.add("endless-mode--invisible");
         classicModeButton.classList.add("classic-mode--invisible");
         rulloLogo.classList.add("rullo-logo--invisible");
-        
+
         this.addEventListener("animationend", hideMenuContainer); // hide main menu container when animation is done
-//        initClassicGame();
+        //        initClassicGame();
     });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
 
     const mainMenu = MainMenu();
+    const classicMenu = new ClassicMenu();
 });
