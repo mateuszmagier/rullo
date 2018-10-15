7281f5620bb65f6b744bf384e541c4b3e46045b9
mateuszmagier <mateuszmagier@bitbucket.org>
2018-10-15 21:15:13 +0200

    Add removing result.

--- js/GameResult.js
+++ js/GameResult.js
@@ -1,37 +1,88 @@
-const GameResultView = function (json) {
+/*
+    id = game-id from LS label,
+    counter - result number of displayed results
+*/
+const GameResultView = function (json, id, counter) {
     var resultElement = null,
+        numberElement = null,
         effectElement = null,
         timeElement = null,
+        datetimeElement = null,
+        removeElement = null,
         model = json;
-    
+
+    function createNumberElement() {
+        let element = document.createElement("div");
+        element.classList.add("result-number");
+        element.innerText = counter;
+
+        numberElement = element;
+    }
+
     function createEffectElement() {
         let element = document.createElement("div");
         element.classList.add("result-effect");
-        if(model.isWin) {
+        if (model.isWin) {
             element.classList.add("win");
         } else {
             element.classList.add("defeat");
         }
-        
+
         effectElement = element;
     }
-    
+
     function createTimeElement() {
         let element = document.createElement("div");
         element.classList.add("result-time");
         element.innerText = model.time;
-        
+
         timeElement = element;
+    }
+
+    function createDatetimeElement() {
+        let element = document.createElement("div");
+        element.classList.add("result-datetime");
+        if (model.datetime !== undefined)
+            element.innerText = model.datetime;
+        else
+            element.innerText = "???";
+        
+        datetimeElement = element;
+    }
+    
+    function createRemoveElement() {
+        let element = document.createElement("i");
+        element.classList.add("result-remove", "far", "fa-trash-alt");
+        
+        element.addEventListener("click", function() {
+            let resultName = "game-" + id;
+            let resultElement = this.parentElement;
+            resultElement.classList.add("removed");
+            localStorage.removeItem(resultName);
+            
+            resultElement.addEventListener("transitionend", function() {
+                resultElement.parentElement.removeChild(resultElement);
+            });
+            
+        });
+        
+        removeElement = element;
     }
 
     function createResultElement() {
         let element = document.createElement("div");
         element.classList.add("result");
-        
+
+        // add number
+        element.appendChild(numberElement);
         // add effect
         element.appendChild(effectElement);
-        //add time
+        // add time
         element.appendChild(timeElement);
+        // add datetime
+        element.appendChild(datetimeElement);
+        // add remove button
+        element.appendChild(removeElement);
 
         resultElement = element;
     };
@@ -39,25 +90,28 @@ const GameResultView = function (json) {
     this.getResultElement = function () {
         return resultElement;
     }
-    
+
+    createNumberElement();
     createEffectElement();
     createTimeElement();
+    createDatetimeElement();
+    createRemoveElement();
     createResultElement();
 };
 
 /*
     creates and returns HTML element that click clears all games stats
 */
-GameResultView.createClearButton = function() {
+GameResultView.createClearButton = function () {
     let button = document.createElement("a");
     button.classList.add("clr-button");
     button.setAttribute("href", "index1.html");
     button.innerText = "wyczyść wyniki";
-    
-    button.addEventListener("click", function(e) {
+
+    button.addEventListener("click", function (e) {
         e.preventDefault();
     });
-    
+
     return button;
 }
 
@@ -80,25 +134,27 @@ const GameResultsHelper = function () {
             let resultLabel; // label of result in LS: game-X
             let resultString; // result fetched from LS in String format
             let resultElement; // HTML element containing result info
+            let nr = 0;
             for (let i = 1; i <= gamesNumber; i++) {
                 resultLabel = "game-" + i;
                 resultString = localStorage.getItem(resultLabel);
                 if (resultString !== null) {
+                    nr++;
                     result = JSON.parse(resultString);
                     results.push(result);
-                    resultView = new GameResultView(result);
+                    resultView = new GameResultView(result, i, nr);
                     resultElement = resultView.getResultElement();
                     resultElements.push(resultElement);
                 }
             }
         }
     }
-    
-    this.createElement = function() {
+
+    this.createElement = function () {
         let element = document.createElement("div");
         element.classList.add("results-group");
-        [].forEach.call(resultElements, function(resultElement) {
-           element.appendChild(resultElement); 
+        [].forEach.call(resultElements, function (resultElement) {
+            element.appendChild(resultElement);
         });
         return element;
     }
